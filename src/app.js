879d039a002f59aa288c159fa8d4a57932af9576
stoca2 <swtocaimaza@gmail.com>
2024-12-17 20:02:19 -0300

    agregamos dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import cors from "cors";
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -8,9 +10,14 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 import mocksRouter from "./routes/mocks.router.js";
 
+dotenv.config(); 
 const app = express();
 const PORT = process.env.PORT||8080;
 const connection = mongoose.connect(process.env.MONGO_URL)
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
